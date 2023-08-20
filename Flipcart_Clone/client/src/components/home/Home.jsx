bf5a237e4ed71e03b289889c21d13232c5e94466
Tanvi <tanvigharbudve@gmail.com>
2023-08-20 19:23:25 +0530

    added background color

--- Flipcart_Clone/client/src/components/home/Home.jsx
+++ Flipcart_Clone/client/src/components/home/Home.jsx
@@ -1,12 +1,21 @@
+import { Box, styled } from "@mui/material";
+
 // components
 import Navbar from "./Navbar";
 import Banner from "./Banner";
+
+const Component = styled(Box)`
+    padding: 10px;
+    background-color: #f2f2f2;
+`;
 
 const Home = () =>{
     return(
         <>
             <Navbar/>
-            <Banner/>
+            <Component>
+                <Banner/>
+            </Component>
         </>
     )
 }
